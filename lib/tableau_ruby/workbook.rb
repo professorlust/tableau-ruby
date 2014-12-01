6c1736447d671a1e603baeb84dae9be0c8d0340f
Jim Sidler <jim@jimsidler.com>
2014-12-01 11:59:52 -0700

    Encode image to base64; add image mime type for reference

--- lib/tableau_ruby/workbook.rb
+++ lib/tableau_ruby/workbook.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 module Tableau
   class Workbook
 
@@ -8,8 +10,6 @@ module Tableau
     def all(site_id, user_id, params={})
       resp = @client.conn.get "/api/2.0/sites/#{site_id}/users/#{user_id}/workbooks" do |req|
         params.each {|k,v| req.params[k] = v}
-        req.params['getThumbnails'] = params[:get_thumbnails].nil? ? false : params[:get_thumbnails]
-        req.params['includeProjects'] = 'true'
         req.headers['X-Tableau-Auth'] = @client.token if @client.token
       end
       data = {workbooks: []}
@@ -18,16 +18,17 @@ module Tableau
         workbook = {id: w["id"], name: w["name"]}
 
         if params[:get_thumbnails]
-          workbook["image"] = @client.conn.get("/api/2.0/sites/#{site_id}/workbooks/#{w['id']}/previewImage").body
-          # workbook["view_name"] = Nokogiri::XML(http_request("/api/2.0/sites/#{@site_id}/workbooks/#{wb['id']}/views", nil, "get").body).css("tsResponse views view").first[:name]
+          resp = @client.conn.get("/api/2.0/sites/#{site_id}/workbooks/#{w['id']}/previewImage") do |req|
+            req.headers['X-Tableau-Auth'] = @client.token if @client.token
+          end
+          workbook["image"] = Base64.encode64(resp.body)
+          workbook["image_mime_type"] = "image/png"
         end
 
         data[:workbooks] << workbook
       end
       data.to_json
     end
-
-
 
   end
 end
